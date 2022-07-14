f24c9c223c491f3c6cb1619e704e17c6dea76860
Tim de Jong <49719019+jongdetim@users.noreply.github.com>
2022-07-14 17:24:06 +0000

    added dataset, research paper, subject, train_model.py: read dataset & add labels

---
+++ train_model.py
@@ -0,0 +1,32 @@
+# %%
+import sys
+import numpy as np
+import pandas as pd
+
+# %% constants
+dataset_path = 'datasets/data-multilayer-perceptron.csv'
+labels = ['id', 'diagnosis', 'mean radius', 'mean texture', 'mean perimiter', 'mean area', 'mean smoothness', 'mean compactness', 'mean concavity', 'mean concave points', 'mean symmetry', 'mean fractal simension',
+          'SE radius', 'SE texture', 'SE perimiter', 'SE area', 'SE smoothness', 'SE compactness', 'SE concavity', 'SE concave points', 'SE symmetry', 'SE fractal simension',
+          'worst radius', 'worst texture', 'worst perimiter', 'worst area', 'worst smoothness', 'worst compactness', 'worst concavity', 'worst concave points', 'worst symmetry', 'worst fractal simension']
+
+# %% read file
+dataset = pd.read_csv(dataset_path, names=labels)
+
+# %%
+dataset.head()
+
+# %%
+
+
+def clean_data(data: pd.DataFrame) -> pd.DataFrame:
+    """adds labels and cleans up data
+
+    Args:
+        data (pd.DataFrame)
+
+    Returns:
+        pd.DataFrame
+    """
+    return data
+
+# %%
