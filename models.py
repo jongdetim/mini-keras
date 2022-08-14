4985c456084ef94909be5f938680037a551d49da
Tim de Jong <49719019+jongdetim@users.noreply.github.com>
2022-08-14 14:21:06 +0000

    added check for empty layers list argument in Sequential class in models.py

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
+from multiprocessing.sharedctypes import Value
 from typing import List, Callable
 import numpy as np
 from layers import Layer
 from loss_functions import *
 
+
 class Sequential:
 
-    def __init__(self, layers: List[Layer], loss_function: Loss):
+    def __init__(self, layers: List[Layer], loss_function: Callable):
         self.layers = layers
         self.loss_function = loss_function
         # self.epochs = epochs
@@ -14,18 +16,23 @@ class Sequential:
         self._check_valid_layers()
 
     def _check_valid_layers(self):
+        if not self.layers:
+            raise ValueError(
+                "layers argument is empty. should contain atleast one layer")
         for layer in self.layers:
             if not issubclass(layer, Layer):
-                raise TypeError("layers argument should be a list of classes that inherit from Layer class")
-        for fan_out, fan_in in zip(self.layers[:-1].dimensions[1], self.layers[1:].dimensions[0]):
-            if fan_out != fan_in:
-                raise ValueError("amount of output neurons of every layer should " + \
-                                        "correspond to amount of input neurons in neXt layer")
+                raise TypeError(
+                    "layers argument should be a list of classes that inherit from Layer class")
+        if len(self.layers) > 1:
+            for fan_out, fan_in in zip(self.layers[:-1].dimensions[1], self.layers[1:].dimensions[0]):
+                if fan_out != fan_in:
+                    raise ValueError("amount of output neurons of every layer should " +
+                                     "correspond to amount of input neurons in next layer")
 
-    def fit(self, X, Y, epochs: int=1000, learning_rate: float=0.01, stochastic: bool=False):
+    def fit(self, X, Y, epochs: int = 1000, learning_rate: float = 0.01, stochastic: bool = False):
         self._gradient_descent(X, Y, epochs, learning_rate, stochastic)
 
-    def _gradient_descent(self, X, Y, epochs: int=1000, learning_rate: float=0.01, stochastic=False):
+    def _gradient_descent(self, X, Y, epochs: int = 1000, learning_rate: float = 0.01, stochastic=False):
         error = 0
         for epoch in range(epochs):
             output = self._forward_propagation(X)
@@ -41,6 +48,6 @@ class Sequential:
             output = layer.forward(output, activation=layer.activation)
         return output
 
-    def _backward_propagation(self, gradient, learning_rate: float=0.01) -> np.array:
+    def _backward_propagation(self, gradient, learning_rate: float = 0.01) -> np.array:
         for layer in reversed(self.layers):
             gradient = layer.backward(gradient, learning_rate)
