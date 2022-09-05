everything seems to work well now for single datapoints. cross entropy & softmax derivatives are correct now

activation_functions.py
@@ -53,38 +53,41 @@ class SoftMax(Activation):
         return z / np.sum(z, axis=0)
 
     @staticmethod
-    def backward2(x: np.array, gradient: np.array) -> np.array:
-        s = x.reshape(-1, 1)
-        gradient = gradient.reshape(-1, 1)
-        return np.dot(np.diagflat(s) - np.dot(s, s.T), gradient)
-        # s = x.reshape(-1, 1)
-        # return np.dot(gradient, np.diagflat(s) - np.dot(s, s.T))
-
-    @staticmethod
     def backward(x: np.array, gradient: np.array) -> np.array:
-        # x = SoftMax.forward(x)
-        x = x.reshape(-1, 1)
-        gradient = gradient.reshape(1, -1)
-        n = np.size(x)
-        print(np.identity(n) - x.T @ x)
-        print(gradient)
-        return np.dot(gradient, np.identity(n) - x.T @ x).T
+        s = SoftMax.forward(x).reshape(-1, 1)
+        return np.dot(np.diagflat(s) - np.dot(s, s.T), gradient)
 
-    @staticmethod
-    def backward3(x: np.array, gradient: np.array) -> np.array:
-        # x = SoftMax.forward(x)
-        x = x.reshape(1, -1)
-        gradient = gradient.reshape(1, -1)
-        n = np.size(x)
-        return np.dot(gradient, np.identity(n) - x.T @ x)
+    # @staticmethod
+    # def backward(x: np.array, gradient: np.array) -> np.array:
+    #     x.reshape(-1, 1)
+    #     I = np.eye(x.shape[0])
+    #     return np.dot(SoftMax.forward(x) * (I - SoftMax.forward(x).T), gradient)
+
+    # @staticmethod
+    # def backward5(x: np.array, gradient: np.array) -> np.array:
+    #     x = SoftMax.forward(x)
+    #     x = x.reshape(-1, 1)
+    #     gradient = gradient.reshape(1, -1)
+    #     n = np.size(x)
+    #     # print(np.identity(n) - x.T @ x)
+    #     # print(gradient)
+    #     return np.dot(gradient, np.identity(n) - x.T @ x).T
+
+    # @staticmethod
+    # def backward3(x: np.array, gradient: np.array) -> np.array:
+    #     x = SoftMax.forward(x)
+    #     # x = x.reshape(-1, 1)
+    #     # gradient = gradient.reshape(1, -1)
+    #     n = np.size(x)
+    #     return np.identity(n) - x.T @ x
     
-    @staticmethod
-    def backward4(x, gradient): # Best implementation (VERY FAST)
-        s = SoftMax.forward(x)
-        a = np.eye(s.shape[-1])
-        temp1 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
-        temp2 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
-        temp1 = np.einsum('ij,jk->ijk',s,a)
-        temp2 = np.einsum('ij,ik->ijk',s,s)
-        print(temp1-temp2)
-        return np.multiply(temp1-temp2, gradient)
+    # @staticmethod
+    # def backward4(x, gradient): # Best implementation (VERY FAST)
+    #     s = SoftMax.forward(x)
+    #     a = np.eye(s.shape[-1])
+    #     temp1 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
+    #     temp2 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
+    #     temp1 = np.einsum('ij,jk->ijk',s,a)
+    #     temp2 = np.einsum('ij,ik->ijk',s,s)
+    #     print(temp1-temp2)
+    #     return np.multiply(temp1-temp2, gradient)

loss_functions.py
@@ -15,22 +15,28 @@ class Loss(ABC):
 
 
 class BinaryCrossEntropy(Loss):
-    # @staticmethod
+    @staticmethod
+    def forward(prediction: np.array, truth: np.array, epsilon=0.000001) -> np.array:
+        prediction = np.clip(prediction, epsilon, 1 - epsilon)
+        size = np.shape(truth)[0] #or [1] ?
+        return -1 / size * np.sum(((truth * np.log(prediction)) + ((1 - truth) * np.log(1 - prediction))))
+
     # def forward(prediction: np.array, truth: np.array, epsilon=0.000001) -> np.array:
     #     prediction = np.clip(prediction, epsilon, 1 - epsilon)
     #     size = np.shape(truth)[0] #or [1] ?
     #     return -1 / size * np.sum(((truth * np.log(prediction)) + ((1 - truth) * np.log(1 - prediction))))
    
-    @staticmethod
-    def forward(y_pre, y):
-        loss=-np.sum(y*np.log(y_pre))
-        return loss/float(y_pre.shape[0])
+    # @staticmethod
+    # def forward(y_pre, y):
+    #     loss=-np.sum(y*np.log(y_pre))
+    #     return loss/float(y_pre.shape[0])
 
     @staticmethod
     def backward(prediction: np.array, truth: np.array, epsilon=0.000001) -> np.array:
-        # return np.array([-0.5, 0.5]).reshape(-1, 1)
+        # return np.array([-0.00001, 0.00001]).reshape(-1, 1)
+        # return -truth/(prediction + 10**-100)
             
         prediction = np.clip(prediction, epsilon, 1 - epsilon)
-        return (prediction - truth) / (prediction * (1 - prediction))
+        return (prediction - truth) / (prediction * (1 - prediction)) / truth.size
         # prediction = np.clip(prediction, epsilon, 1 - epsilon)
         # return (prediction - truth) / (prediction * (1 - prediction)) / np.size(truth)

train_model.py
@@ -23,7 +23,7 @@ model = Sequential([Dense((2, 1), Sigmoid)], BinaryCrossEntropy)
 #%% TEST - IT LEARNS SLOWER WITH ACTIVATED LAYERS! why?
 model = Sequential([Dense((2, 1), activation=ReLU),
                     Dense((1, 1), activation=ReLU),
-                    Dense((1, 1), activation=Sigmoid)], BinaryCrossEntropy)
+                    Dense((1, 2), activation=SoftMax)], BinaryCrossEntropy)
 
 #%% TEST input + 2 layers
 model = Sequential([Dense((2, 4), Sigmoid),
@@ -56,6 +56,12 @@ model = Sequential([Dense((2, 2), Sigmoid),
 model.fit(np.array([0.3, 0.15]).reshape(-1, 1), np.array([1, 0]).reshape(-1, 1), epochs=500, learning_rate=0.01)
 
 #%%
+model._forward_propagation(np.array([0.000000000000000003, 0.0000000000000015]).reshape(-1, 1))
+
+#%%
+SoftMax.forward(np.array([800000, -800000]).reshape(-1, 1))
+
+#%%
 for layer in model.layers:
     print("weights:", layer.weights)
     print("biases:", layer.biases)
@@ -67,16 +73,25 @@ soft = SoftMax.forward(v)
 BinaryCrossEntropy.forward(soft, truth)
 
 #%%
-SoftMax.backward(np.array([0.3, 0.5]), np.array([0.4, -0.6]))
+inputt = np.array([1, 2]).reshape(-1, 1)
+out = SoftMax.forward(inputt)
+truth = np.array([0, 1]).reshape(-1, 1)
+
+loss = BinaryCrossEntropy.forward(out, truth)
+print(SoftMax.backward(inputt, BinaryCrossEntropy.backward(out, truth)))
+print(out - truth)
 
 #%%
-SoftMax.backward2(np.array([0.3, 0.5]), np.array([0.4, -0.6]))
-
-#%%
-SoftMax.backward3(np.array([0.3, 0.5]), np.array([0.4, -0.6]))
-
-#%%
-SoftMax.backward4(np.array([0.3, 0.5]).reshape(-1, 1), np.array([0.4, 0.9]).reshape(-1, 1))
+print(SoftMax.backward(np.array([0.3, 0.5]), np.array([0.4, -0.6])))
+print()
+print(SoftMax.backward2(np.array([0.3, 0.5]), np.array([0.4, -0.6])))
+print()
+print(SoftMax.backward3(np.array([0.3, 0.5]), np.array([0.4, -0.6])))
+print()
+print(SoftMax.backward4(np.array([0.3, 0.5]).reshape(-1, 1), np.array([0.4, 0.9]).reshape(-1, 1)))
+print()
+print(SoftMax.backward5(np.array([0.3, 0.5]).reshape(-1, 1), np.array([0.4, 0.9]).reshape(-1, 1)))
+print()
 
 # %% constants
 dataset_path = 'datasets/data-multilayer-perceptron.csv'