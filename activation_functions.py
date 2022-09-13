softmax.backward4 last einsum is correct

activation_functions.py
@@ -56,8 +56,8 @@ class SoftMax(Activation):
     def backward(x: np.array, gradient: np.array) -> np.array:
         # s = SoftMax.forward(x).reshape(-1, 1)
         s = SoftMax.forward(x)
-        # print(s)
-        # print(np.diagflat(s))
+        print(gradient)
+        print("deriv:", np.diagflat(s) - np.dot(s, s.T))
         # print(x)
         return np.dot(np.diagflat(s) - np.dot(s, s.T), gradient)
 
@@ -84,17 +84,49 @@ class SoftMax(Activation):
     #     # gradient = gradient.reshape(1, -1)
     #     n = np.size(x)
     #     return np.identity(n) - x.T @ x
-    
+
     @staticmethod
-    def backward4(x, gradient): # Best implementation (VERY FAST)
+    def backward4(x, gradient):  # Best implementation (VERY FAST)
         s = SoftMax.forward(x)
         a = np.eye(s.shape[-1])
         # temp1 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
         # temp2 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
-        temp1 = np.einsum('ij,jk->ijk',s,a)
-        temp2 = np.einsum('ij,ik->ijk',s,s)
-        print(temp1-temp2)
-        return np.dot(temp1-temp2, gradient)
+        temp1 = np.einsum('ij,jk->ijk', s, a)
+        temp2 = np.einsum('ij,ik->ijk', s, s)
+        # print(temp1, temp2, temp1-temp2)
+        deriv = np.array(
+            [[[0.20110808, -0.20110808], [-0.20110808, 0.20110808]]])
+        # result = np.einsum('ijk,jk->jk', temp1-temp2, gradient)
+        result = np.einsum('ijk,ki->ji', deriv, gradient)
+        print("deriv:", deriv)
+        print("softmax:", s)
+        print("softmax_derivative:", temp1-temp2)
+        print("gradient:", gradient)
+        print("result:", result)
+        return result
+        # return np.dot(temp1-temp2, gradient)
+
+    @staticmethod
+    def backward7(x, gradient):  # Best implementation (VERY FAST)
+        s = SoftMax.forward(x)
+        a = np.eye(s.shape[-1])
+        # temp1 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
+        # temp2 = np.zeros((s.shape[0], s.shape[1], s.shape[1]),dtype=np.float32)
+        temp1 = np.einsum('ij,kj->ijk', s, a)
+        temp2 = np.einsum('ij,ik->ijk', s, s)
+        # print(temp1, temp2, temp1-temp2)
+        deriv = np.array([[[0.20110808, -0.20110808], [-0.20110808, 0.20110808]],
+                          [[0.3, -0.2], [-0.5, 0.8]]])
+        # result = np.einsum('ijk,jk->jk', temp1-temp2, gradient)
+        print("deriv shape:", deriv.shape)
+        result = np.einsum('ijk,ki->ji', deriv, gradient)
+        print("deriv:", deriv)
+        print("softmax:", s)
+        print("softmax_derivative:", temp1-temp2)
+        print("gradient:", gradient)
+        print("result:", result)
+        return result
+        # return np.dot(temp1-temp2, gradient)
 
     @staticmethod
     def backward6(x, gradient):

train_model.py
@@ -1,3 +1,7 @@
+#%%
+%load_ext autoreload
+%autoreload 2
+
 # %%
 from loss_functions import *
 from activation_functions import *
@@ -5,8 +9,6 @@ from models import Sequential
 from layers import Dense
 import pandas as pd
 import numpy as np
-%load_ext autoreload
-%autoreload 2
 
 # %%
 
@@ -63,22 +65,23 @@ print(x.shape, y.shape)
 # print(x, y)
 
 # %%
-gradient = np.array([[-0.25256681,  0.26752522],
-                     [0.25256681, -0.26752522]])
-x = np.array([[-0.25256681,  0.26752522],
-              [0.25256681, -0.26752522]])
-test = SoftMax.backward6(x, gradient)
-print(test.shape, test)
+gradient = np.array([[-0.5,  0],
+                     [0.8,  0]])
+x = np.array([[-0.25,  0.7],
+              [0, 0]]).T
+test = SoftMax.backward7(x, gradient)
+print(gradient.shape)
 
 #%%
-gradient = np.array([[-0.25256681,  0.26752522]])
-x = np.array([[-0.25256681,  0.26752522]])
+gradient = np.array([[-0.5,  0.8]]).reshape(-1, 1)
+print(gradient.shape)
+x = np.array([[-0.25,  0.7]]).reshape(-1, 1)
 test = SoftMax.backward4(x, gradient)
-print(test.shape, test)
+# print(test.shape, test)
 
 #%%
-gradient = np.array([[-0.25256681,  0.26752522]]).reshape(-1, 1)
-x = np.array([[-0.25256681,  0.26752522]]).reshape(-1, 1)
+gradient = np.array([[-0.5,  0.8]]).reshape(-1, 1)
+x = np.array([[-0.25,  0.7]]).reshape(-1, 1)
 test = SoftMax.backward(x, gradient)
 print(test.shape, test)
 
@@ -119,20 +122,6 @@ truth = np.array([0, 1]).reshape(-1, 1)
 loss = BinaryCrossEntropy.forward(out, truth)
 print(SoftMax.backward(inputt, BinaryCrossEntropy.backward(out, truth)))
 print(out - truth)
-
-# %%
-print(SoftMax.backward(np.array([0.3, 0.5]), np.array([0.4, -0.6])))
-print()
-print(SoftMax.backward2(np.array([0.3, 0.5]), np.array([0.4, -0.6])))
-print()
-print(SoftMax.backward3(np.array([0.3, 0.5]), np.array([0.4, -0.6])))
-print()
-print(SoftMax.backward4(
-    np.array([0.3, 0.5]).reshape(-1, 1), np.array([0.4, 0.9]).reshape(-1, 1)))
-print()
-print(SoftMax.backward5(
-    np.array([0.3, 0.5]).reshape(-1, 1), np.array([0.4, 0.9]).reshape(-1, 1)))
-print()
 
 # %% constants
 dataset_path = 'datasets/data-multilayer-perceptron.csv'