17ca4d2594edd94101f7f5105257007bca9fd67c
Tim de Jong <49719019+jongdetim@users.noreply.github.com>
2022-10-24 14:30:16 +0000

    added predict.py and cleaned up for eval

--- train_model.py
+++ train_model.py
@@ -2,7 +2,6 @@
 # %load_ext autoreload
 # %autoreload 2
 
-# from timeit import timeit
 import pandas as pd
 import numpy as np
 
@@ -18,9 +17,6 @@ dataset_path = 'datasets/data-multilayer-perceptron.csv'
 labels = ['id', 'diagnosis', 'mean radius', 'mean texture', 'mean perimeter', 'mean area', 'mean smoothness', 'mean compactness', 'mean concavity', 'mean concave points', 'mean symmetry', 'mean fractal dimension',
           'SE radius', 'SE texture', 'SE perimeter', 'SE area', 'SE smoothness', 'SE compactness', 'SE concavity', 'SE concave points', 'SE symmetry', 'SE fractal dimension',
           'worst radius', 'worst texture', 'worst perimeter', 'worst area', 'worst smoothness', 'worst compactness', 'worst concavity', 'worst concave points', 'worst symmetry', 'worst fractal dimension']
-
-def clean_data(data: pd.DataFrame) -> pd.DataFrame:
-    return data
 
 def print_full(x):
     pd.set_option('display.max_rows', None)
@@ -38,8 +34,7 @@ def print_full(x):
 # %% read file
 full_dataset = pd.read_csv(dataset_path, names=labels)
 
-#%%
-# dataset = full_dataset[['worst area', 'worst smoothness', 'mean texture', 'diagnosis']]
+#%% preprocess data
 dataset = full_dataset.iloc[:,1:32]
 Y, Y_labels = one_hot(dataset['diagnosis'].to_numpy())
 dataset = dataset.drop('diagnosis', axis=1)
@@ -48,8 +43,6 @@ dataset['Y'] = Y.tolist()
 
 train_set, validation_set = split_dataset(dataset, 0.5, seed=1)
 
-# X_train = train_set[['worst area', 'worst smoothness', 'mean texture']].to_numpy()
-# X_validation = validation_set[['worst area', 'worst smoothness', 'mean texture']].to_numpy()
 X_train = train_set.iloc[:,:-1].to_numpy()
 X_validation = validation_set.iloc[:,:-1].to_numpy()
 Y_train = np.vstack(train_set['Y'].to_numpy())
@@ -57,67 +50,15 @@ Y_validation = np.vstack(validation_set['Y'].to_numpy())
 
 validation_set = {'X': X_validation, 'Y': Y_validation}
 
-#%%
-# x_ = dataset[['worst area', 'worst smoothness', 'mean texture']].to_numpy()
-# X = standardize(X)
-print_full(train_set.iloc[[0]])
-# print(train_set)
-# print(X_train)
-
-# %%
-model = Sequential([Dense((3, 2), activation=SoftMax)], BinaryCrossEntropy)
-
-# %%
-model = Sequential([Dense((3, 5), activation=ReLU),
-                    Dense((5, 2), activation=SoftMax)], BinaryCrossEntropy)
-
 # %%
 model = Sequential([Dense((30, 8), activation=LReLU, seed=1),
                     Dense((8, 5), activation=LReLU, seed=1),
                     Dense((5, 2), activation=SoftMax, seed=1)], BinaryCrossEntropy)
 
-
 #%%
-model.fit(X_train, Y_train, epochs=100, learning_rate=0.01, batch_size=32, validation_set=validation_set, seed=1)
-model.score(X_train, Y_train)
-model.score(X_validation, Y_validation)
-
-# model.fit(X_train, Y_train, epochs=1000, learning_rate=0.01, batch_size=len(X_train), validation_set=validation_set)
-# model.score(X_train, Y_train)
-# model.score(X_validation, Y_validation)
-
-#%%
-model.predict(X_validation, Y_labels, output_type='exclusive')
-# model.predict(X[0:5], Y_labels, output_type='numerical')
-
-#%%
-print(X_train)
-
-# #%%
-# model.score(X, Y)
-
-# %%
-for layer in model.layers:
-    print("weights:", layer.weights)
-    print("biases:", layer.biases)
+model.fit(X_train, Y_train, epochs=700, learning_rate=0.01, batch_size=32, validation_set=validation_set, seed=1)
+print("training set", model.score(X_train, Y_train))
+print("validation set", model.score(X_validation, Y_validation))
 
 #%%
 model.save("MLP", as_json=False)
-
-#%%
-model = Sequential.load("MLP.pickle", as_json=False)
-for layer in model.layers:
-    print("weights:", layer.weights)
-    print("biases:", layer.biases)
-
-#%%
-# import cProfile
-# profile = cProfile.Profile()
-# profile.enable()
-
-# model.fit(X_train, Y_train, epochs=1000, learning_rate=0.01,\
-#     batch_size=32, validation_set=validation_set, seed=1)
-
-# profile.sort_stats('cumtime')
-
-# %%
